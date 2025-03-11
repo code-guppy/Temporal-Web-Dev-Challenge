8674de85b4b34496b0b5a4dc559fa3f785905459
Nikki Meyer <nikki.meyer@codeup.com>
2025-03-11 11:57:42 -0500

    add packages

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-03-04',
+  devtools: { enabled: true },
+  // modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  // modules: ["nuxt-server-utils"],
+  nitro: {
+    plugins: ["~/server/index.ts"],
+    experimental: { websocket: true },
+  },
+
+  runtimeConfig: {
+    mongodbUri: process.env.MONGODB_URI,
+  },
+  css: ["~/assets/main.scss"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  // typescript: {
+  //   typeCheck:true,
+  // }
+
+  // devtools: { enabled: true },
+  // vite: {
+  //   plugins: [
+  //     tailwindcss(),
+  //   ],
+  // },
+});
